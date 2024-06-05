66bcd78d729e203a100fd1ded64d2869414a9ce5
Sadiq Ahmed <sadiq0698@gmail.com>
2024-06-05 10:59:21 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// fallback json response
+Route::fallback(function () {
+    return response()->json([
+        'status' => 'ERR',
+        'message' => 'Access Denied',
+    ], 403, [], JSON_PRETTY_PRINT);
+});
